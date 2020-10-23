1085910f66708827cb065db2f5c7042ded0e8d53
wildan <wildan@adakerja.com>
2020-10-23 18:09:18 +0700

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 require('./mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/', async (_, res) => {
